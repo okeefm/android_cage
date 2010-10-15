f8f511d27f51fa3f4450f5d8b823021e5507c529
Michael O'Keefe <okeefm@rpi.edu>
2010-10-15 11:40:28 -0400

    Updated AccelerometerThread to add connect and disconnect methods

--- src/com/okeefm/android/cage/PointSource.java
+++ src/com/okeefm/android/cage/PointSource.java
@@ -9,6 +9,7 @@ public class PointSource {
 	private AccelerometerThread runLoop;
 	private Exception problem = null;
 	
+	private volatile boolean connected = false;
 	private volatile boolean doConnect = false;
 	private volatile boolean doDisconnect = false;
 	
@@ -33,6 +34,14 @@ public class PointSource {
 		}
 		return points.take();
 	}
+	
+	public void clear() {
+		points.clear();
+	}
+	
+	public boolean isConnected() {
+		return connected;
+	}
 
 	private class AccelerometerThread extends Thread {
 		private Accelerometer acc;
@@ -45,7 +54,28 @@ public class PointSource {
 
 		@Override
 		public void run() {
-			
+			for (;;) {
+				if (doConnect) {
+					doConnect = false;
+					try {
+						acc.connect(listener);
+					} catch(NotConnectedException e) {
+						problem = e;
+					}
+				}
+				
+				if (doDisconnect) {
+					try {
+						if (acc != null) {
+							acc.disconnect(listener);
+						}
+					} catch(NotConnectedException e) {
+						problem = e;
+					}
+					doDisconnect = false;
+				}
+				
+			}
 		}
 	
 		private void setAcc(Context context) {
